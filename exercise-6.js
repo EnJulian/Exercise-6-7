ded62d9b6505665b61d8dc74d6c621ca8c2f9d2c
Julian <36317786+EnJulian@users.noreply.github.com>
2023-07-13 00:16:51 +0000

    Initial Commit

---
+++ exercise-6.js
@@ -0,0 +1,57 @@
+﻿function book() {
+    let age = parseInt(prompt("Enter age: "));
+    let movieType = prompt("Regular, 3D or IMAX? ")
+    let ticketPrice = 0
+    let seatSelection = confirm("Are you in as a VIP?")
+    
+    if (seatSelection === true) {
+        ticketPrice += 5
+    } else ticketPrice = 0;
+
+    if (movieType === "Regular") {
+        if (age < 12) {
+            ticketPrice += 8;
+            console.log(`Ticket price for children is $${ticketPrice}`);
+        } else if (age >= 12 && age < 65) {
+            ticketPrice += 12;
+            console.log(`Ticket price for adults is $${ticketPrice}`);
+        } else if (age >= 65) {
+            ticketPrice += 10;
+            console.log(`Ticket price for seniors is $${ticketPrice}`);
+        }
+        let result = alert(ticketPrice);
+        return result;
+
+    } else if (movieType === "3D") {
+
+        if (age < 12) {
+            ticketPrice += 10;
+            console.log(`Ticket price for children is $${ticketPrice}`);
+        } else if (age >= 12 && age < 65) {
+            ticketPrice += 15;
+            console.log(`Ticket price for adults is $${ticketPrice}`);
+        } else if (age >= 65) {
+            ticketPrice += 12;
+            console.log(`Ticket price for seniors is $${ticketPrice}`);
+        }
+        let result = alert(ticketPrice);
+        return result;
+
+    } else if (movieType === "IMAX") {
+
+        if (age < 12) {
+            ticketPrice += 12;
+            console.log(`Ticket price for children is $${ticketPrice}`);
+        } else if (age >= 12 && age < 65) {
+            ticketPrice += 18;
+            console.log(`Ticket price for adults is $${ticketPrice}`);
+        } else if (age >= 65) {
+            ticketPrice += 15;
+            console.log(`Ticket price for seniors is $${ticketPrice}`);
+        }
+        let result = alert(ticketPrice);
+        return result;
+
+    }
+}
+book();
---
+++ exercise-7.js
@@ -0,0 +1,47 @@
+﻿// function sub() {
+//    
+    let basicAmt = 10;
+    let standardAmt = 15;
+    let premiumAmt = 20;
+    let personalTrainerAmt = 5;
+    let mealPlanAmt = 8;
+    let selectedPlan ="premium";
+    let personalTrainer = true;
+    let mealPlan = true;
+    let subscription = 0;
+
+    switch (selectedPlan) {
+        case "basic":
+            (subscription += basicAmt);
+            break;
+        case "standard":
+            (subscription += standardAmt);
+            break;
+        case "premium":
+            (subscription += premiumAmt);
+            break;
+            
+            // let result = alert(subscription);
+            // return result;
+    }
+    if (personalTrainer === true) {
+        subscription += personalTrainerAmt;
+        // let result = alert(subscription);
+        // return result;
+    }
+    if (mealPlan === true) {
+        subscription += mealPlanAmt;
+        // let result = alert(subscription);
+        // return result;
+    }
+    if (personalTrainer === true && mealPlan === true) {
+        subscription -= 3;
+        // let result = alert(`Total Subscription cost is ${subscription}`);
+        // return result;
+    }
+    
+    console.log(`Your monthly subscription cost is: ${subscription}`);
+    
+// }
+//
+// sub();
